9341e624d2ea45267a2f33f8be3db26071e9b710
Sourav Das <souravdas142@gmail.com>
2019-06-18 17:53:52 +0530

    ./extract.sh: 12: ./extract.sh: cannot create ./scripts/gresource-extract/theme/icons/message-indicator-symbolic.svg: Directory nonexistent    FIXED...

--- gresource-extract/extract.sh
+++ gresource-extract/extract.sh
@@ -2,11 +2,11 @@
 
 workdir=${PWD}
 
-if [ ! -d ${workdir}/theme ]; then
-  mkdir -p ${workdir}/theme
+if [ ! -d ${workdir}/theme/icons ]; then
+  mkdir -p ${workdir}/theme/icons
 fi
 
-gst=/usr/share/gnome-shell/gnome-shell-theme.gresource
+gst=./gnome-shell-theme.gresource
 
 for r in `gresource list $gst`; do
         gresource extract $gst $r >$workdir/${r#\/org\/gnome\/shell/}
