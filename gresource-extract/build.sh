2ccc4f7f3831deffc3b18735aa25cf4ee704df13
Sourav Das <souravdas142@gmail.com>
2019-06-18 17:54:31 +0530

    Some code improvements. XMLLINT not set and xmllint not found in path; skipping xml preprocessing. FIXED . libxml2-utils Dependency

--- gresource-extract/build.sh
+++ gresource-extract/build.sh
@@ -1,17 +1,19 @@
-FILES=$(find "theme" -type f -printf "%P\n" | xargs -i echo "    <file>{}</file>")
+if [ -d theme/icons ]; then
+	cd ./theme/
+	FILES=$(find ./ -type f | sed 's/^..//' | xargs -i echo "    <file>{}</file>")
 
-cat <<EOF >"theme/gnome-shell-theme.gresource.xml"
-<?xml version="1.0" encoding="UTF-8"?>
-<gresources>
-  <gresource prefix="/org/gnome/shell/theme">
-$FILES
-  </gresource>
-</gresources>
+	cat <<EOF >"./gnome-shell-theme.gresource.xml"
+	<?xml version="1.0" encoding="UTF-8"?>
+	<gresources>
+		<gresource prefix="/org/gnome/shell/theme">
+			$FILES
+		</gresource>
+	</gresources>
 EOF
 
-(
-  cd "theme"
-  glib-compile-resources gnome-shell-theme.gresource.xml
-)
+	(
+		glib-compile-resources gnome-shell-theme.gresource.xml
+	)
 
-echo "-> theme/gnome-shell-theme.gresource"
+	echo "-> theme/gnome-shell-theme.gresource"
+fi
